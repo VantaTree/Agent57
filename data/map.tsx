1205d76071f5a3d09be6af5cf6ebdc3a4e4c63ee
VantaTree <vantatree@gmail.com>
2024-03-02 18:43:00 +0530

    updated maps

--- data/map.tsx
+++ data/map.tsx
@@ -3,24 +3,20 @@
  <image source="../graphics/map/tileset.png" width="192" height="304"/>
  <wangsets>
   <wangset name="ground" type="corner" tile="-1">
-   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
-   <wangtile tileid="76" wangid="0,0,0,1,0,0,0,0"/>
-   <wangtile tileid="77" wangid="0,0,0,1,0,1,0,0"/>
-   <wangtile tileid="78" wangid="0,0,0,0,0,1,0,0"/>
-   <wangtile tileid="79" wangid="0,0,0,1,0,0,0,0"/>
-   <wangtile tileid="80" wangid="0,0,0,0,0,1,0,0"/>
-   <wangtile tileid="88" wangid="0,1,0,1,0,0,0,0"/>
+   <wangcolor name="main" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="81" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="82" wangid="0,0,0,1,0,1,0,1"/>
    <wangtile tileid="89" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="90" wangid="0,0,0,0,0,1,0,1"/>
-   <wangtile tileid="91" wangid="0,1,0,0,0,0,0,0"/>
-   <wangtile tileid="92" wangid="0,0,0,0,0,0,0,1"/>
-   <wangtile tileid="100" wangid="0,1,0,0,0,0,0,0"/>
-   <wangtile tileid="101" wangid="0,1,0,0,0,0,0,1"/>
-   <wangtile tileid="102" wangid="0,0,0,0,0,0,0,1"/>
-   <wangtile tileid="116" wangid="0,1,0,1,0,1,0,0"/>
-   <wangtile tileid="117" wangid="0,0,0,1,0,1,0,1"/>
-   <wangtile tileid="128" wangid="0,1,0,1,0,0,0,1"/>
-   <wangtile tileid="129" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="93" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="94" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="112" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="113" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="114" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="124" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="126" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="136" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="137" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="138" wangid="0,0,0,0,0,0,0,1"/>
   </wangset>
  </wangsets>
 </tileset>
